4301088223464c565eb3a8abf3ed4b8faf4bb05b
IamLoWLo <iamlowlo@gmail.com>
2018-02-25 13:35:50 +0100

    Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AppRoutes } from './routes.config';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './containers/welcome/welcome.component';
 import { AdminComponent } from './containers/admin/admin.component';
 import { LoginComponent } from './containers/login/login.component';
-import { AppRoutes } from './routes.config';
 
 
 
@@ -22,7 +22,7 @@ const firebaseConfig = {
     databaseURL: 'https://wed-nvttn-roccar.firebaseio.com',
     storageBucket: 'wed-nvttn-roccar.appspot.com',
     messagingSenderId: '282300012202'
-}
+};
 
 
 @NgModule({
