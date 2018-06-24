f9a934fa5f24a22be738a23c9423308463648e2a
IamLoWLo <iamlowlo@gmail.com>
2018-06-24 21:12:36 +0200

    Added some style changes on gmaps component relative to searchbox

--- src/app/components/gmap/gmap.component.ts
+++ src/app/components/gmap/gmap.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, HostBinding, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { } from '@types/googlemaps';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-gmap',
@@ -7,30 +8,25 @@ import { } from '@types/googlemaps';
   styleUrls: ['./gmap.component.scss']
 })
 export class GmapComponent implements OnInit {
-  public map: google.maps.Map;
-  @ViewChild('gmap') gmap: ElementRef;
   private _latLng: google.maps.LatLng;
+  public isSearchboxOpened: Boolean = false;
+  public map: google.maps.Map;
+  public _height = '500px';
+  public _width = '100%';
+  public searchForm = new FormGroup({
+    address: new FormControl('')
+  });
+  @ViewChild('gmap') gmap: ElementRef;
   @Input()
   private set latLng(latLng: {lat: number, lng: number}) {
     this._latLng = new google.maps.LatLng(latLng.lat, latLng.lng);
   }
-  public _height = '500px';
-  public _width = '100%';
   @Input()
-  public set height(height: string) {
-    this._height = height;
-  }
-  public get height(): string {
-    return this._height;
-  }
+  public set height(height: string) { this._height = height; }
+  public get height(): string { return this._height; }
   @Input()
-  public set width(width: string) {
-    this._width = width || '100%';
-  }
-  public get width(): string {
-    console.log(this._width);
-    return this._width;
-  }
+  public set width(width: string) { this._width = width || '100%'; }
+  public get width(): string { return this._width;}
 
   constructor() { }
 
@@ -38,10 +34,22 @@ export class GmapComponent implements OnInit {
     this.map = new google.maps.Map(this.gmap.nativeElement, {
       center: new google.maps.LatLng(this._latLng.lat(), this._latLng.lng()),
       zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
     });
     const marker = new google.maps.Marker({position: this._latLng});
     marker.setMap(this.map);
+  }
+  toggleSearchbox() {
+    this.isSearchboxOpened = !this.isSearchboxOpened;
+  }
+
+  onFocus($event) {
+    $event.target.classList.add('ng-focused');
+  }
+
+  onBlur($event) {
+    $event.target.classList.remove('ng-focused');
   }
 
 }
