da547be2414e774e09cb116be9d5b1a6bdee30e1
IamLoWLo <iamlowlo@gmail.com>
2018-07-07 21:28:40 +0200

    Added all gallery images and party content, first styles and sound tag

--- src/app/containers/landing/containers/party/party.container.ts
+++ src/app/containers/landing/containers/party/party.container.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-party',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartyContainer implements OnInit {
 
+  @ViewChild('audio') audio: ElementRef;
+  private isPlayingAudio: Boolean = true;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  pauseMusic() {
+    if (this.isPlayingAudio) {
+      this.audio.nativeElement.pause();
+    } else {
+      this.audio.nativeElement.play();
+    }
+    this.isPlayingAudio = !this.isPlayingAudio;
+  }
 }
